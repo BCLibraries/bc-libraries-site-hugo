246b9e8ec311013bfeccecdd803696a4daa66e9f
Jesse Martinez <mjesse@gmail.com>
2019-10-03 11:17:54 -0400

    updating bento
    Applied changes from bc-libraries-site commits through 60232817

---
+++ themes/BC/static/bcbento/precache-manifest.37b96a350a0d47310bc96822329838b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4caa839b0782fd30a881",
+    "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "31dc911a3b6bb3d486d1",
+    "url": "../theme/bcbento/static/js/main.3bdf7233.chunk.js"
+  },
+  {
+    "revision": "5f77e06126d4a1d81054",
+    "url": "../theme/bcbento/static/js/2.36c591c8.chunk.js"
+  },
+  {
+    "revision": "051371ca4dccef991fb488ad6f1d7606",
+    "url": "../theme/bcbento/index.html"
+  }
+];
--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../bcbento/precache-manifest.099ce1a6c1793d8f119928f2c06ac9a3.js"
+  "../theme/bcbento/precache-manifest.37b96a350a0d47310bc96822329838b1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("../bcbento/index.html", {
+workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
