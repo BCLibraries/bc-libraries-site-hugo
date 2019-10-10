8feef021d59a0963e23a1dcfd8bff471d932492a
Benjamin Florin <benjamin.florin@gmail.com>
2019-10-10 18:08:48 -0400

    Connect fulltext access Bento

---
+++ themes/BC/static/bcbento/precache-manifest.02d8fb6f3d67b4207c0d199684499776.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4caa839b0782fd30a881",
+    "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "5cd82d4906ab68c9a783",
+    "url": "../theme/bcbento/static/js/main.ed954e15.chunk.js"
+  },
+  {
+    "revision": "085f51a48a63fec3b01f",
+    "url": "../theme/bcbento/static/js/2.8207eb23.chunk.js"
+  },
+  {
+    "revision": "51eb59d63e56969b0a45423958df54e0",
+    "url": "../theme/bcbento/index.html"
+  }
+];
--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../bcbento/precache-manifest.37b96a350a0d47310bc96822329838b1.js"
+  "../theme/bcbento/precache-manifest.02d8fb6f3d67b4207c0d199684499776.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("../bcbento/index.html", {
+workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
