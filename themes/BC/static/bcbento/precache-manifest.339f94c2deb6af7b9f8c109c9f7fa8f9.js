173862da7b73180e317cc46efb3571566a229c89
Benjamin Florin <benjamin.florin@gmail.com>
2019-10-21 15:03:22 -0400

    Deploy latest bento version

---
+++ themes/BC/static/bcbento/precache-manifest.339f94c2deb6af7b9f8c109c9f7fa8f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4caa839b0782fd30a881",
+    "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "886d5ac3cc89ede1672b",
+    "url": "../theme/bcbento/static/js/main.e6f5ce39.chunk.js"
+  },
+  {
+    "revision": "f1ae29af0d7ebe207314",
+    "url": "../theme/bcbento/static/js/2.bdbe38ef.chunk.js"
+  },
+  {
+    "revision": "f9baf39bc16a070d2912883918f7d93f",
+    "url": "../theme/bcbento/index.html"
+  }
+];
