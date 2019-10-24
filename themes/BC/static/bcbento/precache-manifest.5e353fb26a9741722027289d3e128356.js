b662ce8f3acdf6bc914464e920eb44e2255c8ccd
Luke Gaudreau <gaudreal@bc.edu>
2019-10-24 12:38:25 -0400

    Fix previous commit: Add bento files
    Missing from previous commit

---
+++ themes/BC/static/bcbento/precache-manifest.5e353fb26a9741722027289d3e128356.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "16abf4cf6c4003d486f9",
+    "url": "../theme/bcbento/static/js/main.8a055758.chunk.js"
+  },
+  {
+    "revision": "4caa839b0782fd30a881",
+    "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "085f51a48a63fec3b01f",
+    "url": "../theme/bcbento/static/js/2.8207eb23.chunk.js"
+  },
+  {
+    "revision": "2128c6bd194a25f9f429476f690fbf54",
+    "url": "../theme/bcbento/index.html"
+  }
+];
