b2002c44f0b2d2c7a489c2896ddf7f512ba3f503
Luke Gaudreau <gaudreal@bc.edu>
2019-10-24 12:16:37 -0400

    Bento updates

--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../theme/bcbento/precache-manifest.611dca7bbd9d254722209acb116a3fe9.js"
+  "../theme/bcbento/precache-manifest.12c9ec4f71c2b7cb8ef0e191af045883.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
