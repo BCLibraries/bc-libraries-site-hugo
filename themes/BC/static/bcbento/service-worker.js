f1ee533076ee0cb682d703eadb3363a5847b4939
Jesse Martinez <mjesse@gmail.com>
2019-11-01 09:52:01 -0400

    updating bento link locations

--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../theme/bcbento/precache-manifest.2bac167ba8ae29716a86dd1d7f3c34fe.js"
+  "../bcbento/precache-manifest.2bac167ba8ae29716a86dd1d7f3c34fe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
+workbox.routing.registerNavigationRoute("../bcbento/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
