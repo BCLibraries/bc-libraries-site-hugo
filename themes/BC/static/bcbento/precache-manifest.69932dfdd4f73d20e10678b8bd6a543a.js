f117aba99948a480d5c2775f7aef8955c789f828
Luke Gaudreau <gaudreal@bc.edu>
2019-11-22 10:02:00 -0500

    Add feedback link

---
+++ themes/BC/static/bcbento/precache-manifest.69932dfdd4f73d20e10678b8bd6a543a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a941caca23e5810f8c",
+    "url": "../theme/bcbento/static/js/main.c3d30923.chunk.js"
+  },
+  {
+    "revision": "4caa839b0782fd30a881",
+    "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "285902ae15c31d4fa51e",
+    "url": "../theme/bcbento/static/js/2.6509eec2.chunk.js"
+  },
+  {
+    "revision": "6cdbd47a3bcc699592a042aed7acea41",
+    "url": "../theme/bcbento/index.html"
+  }
+];
--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../theme/bcbento/precache-manifest.6ce91c23af5e048baa5698b5830e66d3.js"
+  "../theme/bcbento/precache-manifest.69932dfdd4f73d20e10678b8bd6a543a.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
