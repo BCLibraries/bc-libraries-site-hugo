96cf780ba6b8e261d9a560f28534c7b4a9dd75b5
Benjamin Florin <benjamin.florin@gmail.com>
2019-12-04 13:23:43 -0500

    Memoize bento results to prevent excessive re-renders

--- themes/BC/static/bcbento/precache-manifest.c9047fc13491d251c0871a1d43637ac4.js
+++ themes/BC/static/bcbento/precache-manifest.c9047fc13491d251c0871a1d43637ac4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "c4a46027d042f8473b6f",
-    "url": "../theme/bcbento/static/js/main.403c060b.chunk.js"
-  },
-  {
     "revision": "4caa839b0782fd30a881",
     "url": "../theme/bcbento/static/js/runtime~main.1cf1c718.js"
+  },
+  {
+    "revision": "e16f86501c6b2bdf7842",
+    "url": "../theme/bcbento/static/js/main.3594e047.chunk.js"
   },
   {
     "revision": "035ccf1d09d21f9ee3a0",
     "url": "../theme/bcbento/static/js/2.3db90434.chunk.js"
   },
   {
-    "revision": "c80d5f09f440d38bb9328ff568562e97",
+    "revision": "0162be29d4144e9f6b81cfaabdde70db",
     "url": "../theme/bcbento/index.html"
   }
 ];
--- themes/BC/static/bcbento/service-worker.js
+++ themes/BC/static/bcbento/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "../theme/bcbento/precache-manifest.1432615522981c43038f06ca438d9722.js"
+  "../theme/bcbento/precache-manifest.c9047fc13491d251c0871a1d43637ac4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("../theme/bcbento/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
