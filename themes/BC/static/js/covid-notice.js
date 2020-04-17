862f275ceb2c48288ba44ca99d0f1c0f44681019
Benjamin Florin <benjamin.florin@gmail.com>
2020-04-17 13:22:59 -0400

    Fix mobile notice close button

--- themes/BC/static/js/covid-notice.js
+++ themes/BC/static/js/covid-notice.js
@@ -1,58 +1,92 @@
-const noticeHTML = '<div class="covid-notice__opener">' +
-    '<button class="covid-notice__open-button">' +
+(function(){
+    console.log('running in here');
+})();
+
+(function () {
+    console.log('running');
+    const cookie = ShowCOVIDNoticeCookie();
+
+    if (cookie.isValidForThisDomain()) {
+        runWhenReady(showCOVID19Notice);
+    }
+
+    function runWhenReady(fn) {
+        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    function showCOVID19Notice() {
+
+        const notice = document.createElement('div');
+        notice.innerHTML = '<div class="covid-notice__opener">' +
+        '<button class="covid-notice__open-button">' +
         '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>' +
         '<span class="sr-only">Show COVID-19 notice</span>' +
-    '</button>' +
-'</div>' + 
-'<div class="covid-notice covid-notice--hidden">' +
-    '<a href="https://libguides.bc.edu/servicesupdate">BC Libraries Services Update - COVID-19</a>' +
-    '<div class="closed-notice" style="font-size: 16px">All BC Libraries are closed. However, <a href="https://libguides.bc.edu/servicesupdate">remote services remain operational</a>.' +
-    '</div>' +
-    '<button class="covid-notice__close-button">Close</button>' +
-'</div>';
+        '</button>' +
+        '</div>' +
+        '<div class="covid-notice covid-notice--hidden">' +
+        '<a href="https://libguides.bc.edu/servicesupdate">BC Libraries Services Update - COVID-19</a>' +
+        '<div class="closed-notice" style="font-size: 16px">All BC Libraries are closed. However, <a href="https://libguides.bc.edu/servicesupdate">remote services remain operational</a>.' +
+        '</div>' +
+        '<button class="covid-notice__close-button">Close</button>' +
+        '</div>';
+        document.body.append(notice);
 
-$(function () {
-    const hostname = window.location.hostname.replace(/^[^\.]*\./,'.');
+        const banner = document.querySelector('.covid-notice');
+        const opener = document.querySelector('.covid-notice__opener');
+
+        if (!cookie.noticeIsClosed()) {
+            showBanner();
+        }
+
+        document.querySelector('.covid-notice__close-button').addEventListener('click', hideBanner, false);
+        document.querySelector('.covid-notice__open-button').addEventListener('click', showBanner, false);
+        
+        function showBanner() {
+            banner.className = 'covid-notice';
+            opener.className = 'covid-notice__opener covid-notice--hidden';
+            cookie.set(true);
+        }
+    
+        function hideBanner() {
+            banner.className = 'covid-notice  covid-notice--hidden';
+            opener.className = 'covid-notice__opener';
+            cookie.set(false);
+        }
+    }
+})();
+
+function ShowCOVIDNoticeCookie() {
+
+    const hostname = window.location.hostname.replace(/^[^\.]*\./, '.');
+    const allowedDomains = ['.bc.edu', 'localhost', '.hosted.exlibrisgroup.com'];
     const cookieName = 'bcl-note-open';
 
-    if ((hostname.toLowerCase().indexOf("bc.edu") !== -1) ||
-        (hostname.toLowerCase().indexOf("localhost") !== -1)) {
-        $('body').append(noticeHTML);
-    }
-
-    const $closeCov19NoticeButton = $('.covid-notice__close-button');
-    const $openCov19NoticeButton = $('.covid-notice__open-button');
-
-    if (! noticeIsClosed()) {
-        toggleNotice();
-    }
-
-    $closeCov19NoticeButton.click(function() {
-        toggleNotice();
-        setCookie(false);
-    })
-
-    $openCov19NoticeButton.click(function() {
-        toggleNotice();
-        setCookie(true);
-    })
-
-    function toggleNotice() {
-        $('.covid-notice').toggleClass('covid-notice--hidden');
-        $('.covid-notice__opener').toggleClass('covid-notice--hidden');
-    }
-
-    function setCookie(status) {
+    function set(status) {
         const value = status ? 1 : 0;
         const expiresAt = new Date();
         expiresAt.setMonth(expiresAt.getMonth() + 3);
         document.cookie = cookieName + "=" + value + ";expires=" + expiresAt + ";domain=" + hostname + ";path=/";
-        console.log(document.cookie);
     }
 
     function noticeIsClosed() {
-        return document.cookie.split(';').some(function(item) {
+        return document.cookie.split(';').some(function (item) {
             return item.indexOf(cookieName + '=0') >= 0
         });
     }
-});
+
+    function isValidForThisDomain() {
+        return allowedDomains.some(function (domain) {
+            return domain === hostname;
+        });
+    }
+
+    return {
+        set: set,
+        noticeIsClosed: noticeIsClosed,
+        isValidForThisDomain: isValidForThisDomain
+    };
+}
