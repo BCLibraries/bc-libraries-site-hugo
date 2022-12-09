43adceca904f5f54ef070b7620dde0182064ae39
Benjamin Florin <benjamin.florin@gmail.com>
2022-12-09 18:48:21 -0500

    Allow to change library hours displayed without changing HTML

--- themes/BC/assets/js/libhoursv2.js
+++ themes/BC/assets/js/libhoursv2.js
@@ -1,69 +1,111 @@
 /* FETCH LIBRARY HOURS */
 
 $(document).ready(function(){
-    // list of library names as keys
-    // dom class name as values
-    var library_list = {
-                "O'Neill Library": ".lib-oneill",
-                "Bapst Library": ".lib-bapst",
-                "Burns Library": ".lib-burns",
-                "Educational Resource Center": ".lib-erc",
-                "Law Library": ".lib-law",
-                "Social Work Library": ".lib-swl",
-                "Theology and Ministry Library": ".lib-tml"
-                };
 
     // turn off/on showing department hours e.g., Gargan Hall
     const SHOW_DEPARTMENTS = true;
 
-    // list of departments
-    var department_list = {
-        "Gargan Hall": ".lib-bapst-gargan"
-    }
+    // jQuery selector for the boxes containing the hours
+    const HOURS_TABLE_SELECTOR = '.table-hours table tbody';
 
-    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-    function showHours(location, lib_list){
-        var rendered = location.rendered;
-        if (location.rendered.length >= 1) {
-            rendered = "<span class='font-small'>" + rendered + "</span>";
-        }
-
-        if (location.name == "O'Neill Library" && location.rendered.includes('*')) {
-            $('.onl-asterisk-notice').show();
-        }
-
-        // libraries with an attached note to the hours will render
-        // the hours string, then a new line char, then the note string.
-        // we will replace the new line char with a semicolon.
-        $(lib_list[location.name]).html(rendered.replace("\n", " ; "));
-    }
+    // the locations we want, in order
+    const locations_to_show = [
+        new Location("501", "O'Neill Library", "https://libguides.bc.edu/oneill/hours", "library"),
+        new Location("505", "Bapst Library", "https://libguides.bc.edu/bapst/hours", "library"),
+        new Location("506", "Bapst Library - Gargan Hall", "https://libguides.bc.edu/bapst/hours", "department"),
+        new Location("507", "Burns Library", "https://libguides.bc.edu/burns", "library"),
+        new Location("508", "Educational Resource Center", "https://libguides.bc.edu/erc/hours", "library"),
+        new Location("509", "Law Library", "https://www.bc.edu/bc-web/schools/law/sites/students/library/using/hours.html", "library"),
+        new Location("510", "Social Work Library", "https://libguides.bc.edu/socialwork/hours", "library"),
+        new Location("511", "Theology & Ministry Library", "https://libguides.bc.edu/tml/hours", "library")
+    ].filter(loc => { return SHOW_DEPARTMENTS || loc.category !== 'department' });
 
     // grab each library's data from the output and place it where it needs to go
     function setHours(data){
-        // set today's date
-        var today = new Date();
-        var year = today.getFullYear();
-        var month = today.getMonth();
-        var day = today.getDate()
-        var date = months[month] + " " + day + ", " + year;
-        $(".hours-todays-date").text(date);
 
-        if ("locations" in data) {
-            $.each(data["locations"], function(index, loc) {
-                var cat = loc.category;
-                var name = loc.name;
-
-                if ((cat == "library") && (name in library_list)) {
-                    showHours(loc, library_list);
-                } else if (SHOW_DEPARTMENTS && (cat == "department") && (name in department_list)) {
-                    showHours(loc, department_list);
-                }
-            });
-        } else {
-            // TODO: Show some sort of error message
+        // no locations? for now, just die early.
+        if (! 'locations' in data) {
+            return;
         }
+
+        // convert data into hash keyed by location id for easier querying.
+        const location_map = {};
+        data.locations.forEach(loc => {
+            location_map[loc.lid] = loc;
+        });
+
+        // create a <tbody> that will hold the hours
+        const new_tbody = document.createElement('tbody');
+
+        // scroll through the locations we want to show, find them in the data, and 
+        // render them
+        locations_to_show.forEach(loc_to_show => {
+
+            // skip locations that aren't in the data
+            if (! location_map[loc_to_show.id]) {
+                console.error(`Hours for ${loc_to_show.label} (${loc_to_show.id}) were not retrieved`);
+                const error_tr = buildRow('Error retrieving hours', loc_to_show.label, loc_to_show.url);
+                new_tbody.appendChild(error_tr)
+                return;
+            }
+
+            // get the data for this location from the hash
+            const location_data = location_map[loc_to_show.id];
+            
+            if (location_data.name == "O'Neill Library" && location_data.rendered.includes('*')) {
+                $('.onl-asterisk-notice').show();
+            }
+
+            // libraries with an attached note to the hours will render
+            // the hours string, then a new line char, then the note string.
+            // we will replace the new line char with a semicolon.
+            const hours = location_data.rendered.replace("\n", " ; ");
+
+            const new_tr = buildRow(hours, loc_to_show.label, loc_to_show.url);
+            new_tbody.appendChild(new_tr)
+        });
+
+        // clone the new <tbody> into the hours tables
+        document.querySelectorAll(HOURS_TABLE_SELECTOR).forEach(old_tbody => {
+            const new_tbody_clone = new_tbody.cloneNode(true);
+            old_tbody.parentNode.replaceChild(new_tbody_clone, old_tbody);
+        });
     };
+
+    // build one row of the months table
+    function buildRow(hours, label, url) {
+
+        const link = document.createElement("a");
+        link.setAttribute("a", url);
+        link.textContent = label;
+
+        const label_cell = document.createElement("td");
+        label_cell.append(link);
+
+        const hours_cell = document.createElement("td");
+        hours_cell.classList.add("lib-hours");
+        hours_cell.textContent = hours;
+
+        const row = document.createElement("tr");
+        row.append(label_cell);
+        row.append(hours_cell);
+
+        return row;
+    }
+
+    function Location(id, label, url, category) {
+        this.id = id;
+        this.label = label;
+        this.url = url;
+        this.category = category;
+    }
+
+    // set today's date
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const today = new Date();
+    const month = months[today.getMonth()];
+    const date = month + " " + today.getDate() + ", " + today.getFullYear();
+    $(".hours-todays-date").text(date);
 
     // call to the hours api
     $.ajax({
@@ -77,8 +119,8 @@ $(document).ready(function(){
         headers: {'Cache-Control': 'max-age=60'},
         success: setHours,
         error: function(){
-            console.log("could not retrieve hours!");
-            $.each("div.mini_hours_container").html("Error retrieving hours");
+            console.error("could not retrieve hours!");
+            $(".table-hours tbody tr td:last-child").html("Error retrieving hours");
         },
     });
 });
