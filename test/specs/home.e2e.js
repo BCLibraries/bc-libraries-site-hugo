d08ee3cb39320b73e688d29bf80e089f2abc071f
Benjamin Florin <benjamin.florin@gmail.com>
2023-04-04 11:48:38 -0400

    Add seasonally-appropriate hero image check

--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -1,8 +1,18 @@
-const HomePage = require('../pageobjects/home.page')
+const HomePage = require('../pageobjects/home.page');
+const assert = require('assert');
+const currentSeason = require('../util/currentSeason');
 
 describe('the home page', () => {
     it('should have a search box', async () => {
-        await HomePage.open()
-        await expect(HomePage.searchInput).toBeExisting()
-    })
+        await HomePage.open();
+        await expect(HomePage.searchInput).toBeExisting();
+    });
+
+    it('should have a seasonally appropriate hero image', async () => {
+        const expectedSeason = currentSeason();
+        await HomePage.open();
+        const heroImage = await HomePage.heroBox.getCSSProperty('background-image');
+        console.log(expectedSeason);
+        await assert(heroImage.value.includes(expectedSeason));
+    });
 })
