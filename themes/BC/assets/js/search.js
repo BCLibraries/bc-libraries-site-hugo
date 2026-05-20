43addaa3f4cd49fb691602d3dc4c62d7492d3218
Benjamin Florin <benjamin.florin@gmail.com>
2026-05-20 14:14:45 -0400

    Fix links to NDE

--- themes/BC/assets/js/search.js
+++ themes/BC/assets/js/search.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const searchForm = document.getElementById('search-panel__form');
-  const queryInputElement = searchForm.querySelector('input#lib-search-box');
-  const querySubmissionElement = searchForm.querySelector('input[name=query]');
-  searchForm.addEventListener('submit', (e) => {
-  })
+    const searchPanel = document.getElementById('search-panel');
+    const booksLink = searchPanel.querySelector('.nde-book-search-link');
+    const articlesLink = searchPanel.querySelector('.nde-article-search-link');
+    const journalsLink = searchPanel.querySelector('.nde-journal-search-link');
+
+    const queryInputElement = searchPanel.querySelector('input#lib-search-box');
+    booksLink.addEventListener('click', handleLinkClick);
+    articlesLink.addEventListener('click', handleLinkClick);
+    journalsLink.addEventListener('click', handleLinkClick);
+
+    /**
+     * Add the search string to a clicked link
+     *
+     * @param {Event} e
+     */
+    function handleLinkClick(e) {
+        const link = e.target;
+        const url = new URL(link.href);
+        url.searchParams.set('query', queryInputElement.value);
+        link.href = url.toString();
+    }
 });
