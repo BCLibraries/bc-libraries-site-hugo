bc324250d64c4e99500336c474d2d4ce33cd1741
Benjamin Florin <benjamin.florin@gmail.com>
2026-05-20 08:58:06 -0400

    Move search to NDE

---
+++ themes/BC/assets/js/search.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchForm = document.getElementById('search-panel__form');
+  const queryInputElement = searchForm.querySelector('input#lib-search-box');
+  const querySubmissionElement = searchForm.querySelector('input[name=query]');
+  searchForm.addEventListener('submit', (e) => {
+  })
+});
