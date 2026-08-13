487b4e8545096b05e00d7fb91170af792c53d240
Benjamin Florin <benjamin.florin@gmail.com>
2026-08-13 14:49:29 -0400

    Fix search script errors on pages without search panels

--- themes/BC/assets/js/search.js
+++ themes/BC/assets/js/search.js
@@ -1,23 +1,27 @@
+// Clicking search categories ('Books', 'Articles') in the search panel should link to the
+// appropriate search, with any text in the search box used as the keyword.
 document.addEventListener('DOMContentLoaded', () => {
-    const searchPanel = document.getElementById('search-panel');
-    const booksLink = searchPanel.querySelector('.nde-book-search-link');
-    const articlesLink = searchPanel.querySelector('.nde-article-search-link');
-    const journalsLink = searchPanel.querySelector('.nde-journal-search-link');
+    const searchPanels = document.querySelectorAll('#search-panel');
 
-    const queryInputElement = searchPanel.querySelector('input#lib-search-box');
-    booksLink.addEventListener('click', handleLinkClick);
-    articlesLink.addEventListener('click', handleLinkClick);
-    journalsLink.addEventListener('click', handleLinkClick);
+    searchPanels.forEach(searchPanel => {
+        const booksLink = searchPanel.querySelector('.nde-book-search-link');
+        const articlesLink = searchPanel.querySelector('.nde-article-search-link');
+        const journalsLink = searchPanel.querySelector('.nde-journal-search-link');
+        const queryInputElement = searchPanel.querySelector('input#lib-search-box');
+        booksLink.addEventListener('click', handleLinkClick);
+        articlesLink.addEventListener('click', handleLinkClick);
+        journalsLink.addEventListener('click', handleLinkClick);
 
-    /**
-     * Add the search string to a clicked link
-     *
-     * @param {Event} e
-     */
-    function handleLinkClick(e) {
-        const link = e.target;
-        const url = new URL(link.href);
-        url.searchParams.set('query', queryInputElement.value);
-        link.href = url.toString();
-    }
+        /**
+         * Add the search string to a clicked link
+         *
+         * @param {Event} e
+         */
+        function handleLinkClick(e) {
+            const link = e.target;
+            const url = new URL(link.href);
+            url.searchParams.set('query', queryInputElement.value);
+            link.href = url.toString();
+        }
+    });
 });
